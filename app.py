e24785d7edd849b9ce1bad895337355759f7688f
fabio vinicius <fabiovinicius1@outlook.com>
2019-10-31 09:45:19 -0400

    Adicionado docstring e tela de login foram adicionados os docstring das classe e metodos que iniciam a tela e que controla os eventos da tela, e tambem foram criadas as telas de login do cliente, adiministrado e bibliotecario
    Signed-off-by: fabio vinicius <fabiovinicius1@outlook.com>

--- app.py
+++ app.py
@@ -1,61 +1,92 @@
-from tela import Tela_inicial, Tela_administrador, Tela_bibliotecario, Tela_cliente
+from tela import Tela_inicial, Tela_administrador_login, Tela_bibliotecario_login, Tela_cliente_login, Tela_cliente,Tela_administrador,Tela_bibliotecario
 from usuario import Usuario
 from administrador import Administrador
 from cliente import Cliente
 from bibliotecario import Bibliotecario
 
 class Controle_tela_administrador():
+    '''Classe para controlar a tela do administrador depois de feito o login'''
     def __init__(self):
         self.tela_administrador = Tela_administrador()
-    
-    def voltar_tela(self):
-        self.tela_administrador.janela_administrador.destroy()
     def start(self):
-        self.tela_administrador.btn_voltar_tela.configure(command = self.voltar_tela)
+        '''Inicia a tela do administrador e os eventos dos botoes'''
         self.tela_administrador.iniciar()
 
 class Controle_tela_bibliotecario():
+    '''Classe para controlar a tela do bibliotecario depois de feito o login'''
     def __init__(self):
         self.tela_bibliotecario = Tela_bibliotecario()
-    
-    def voltar_tela(self):
-        self.tela_bibliotecario.janela_bibliotecario.destroy()
     def start(self):
-        self.tela_bibliotecario.btn_voltar_tela.configure(command = self.voltar_tela)
+        '''Inicia a tela do bibliotecario e os eventos dos botoes'''
         self.tela_bibliotecario.iniciar()
 
 class Controle_tela_cliente():
+    '''Classe para controlar a tela do cliente depois de feito o login'''
     def __init__(self):
         self.tela_cliente = Tela_cliente()
-    
-    def voltar_tela(self):
-        self.tela_cliente.janela_cliente.destroy()
     def start(self):
-        self.tela_cliente.btn_voltar_tela.configure(command = self.voltar_tela)
+        '''Inicia a tela do cliente e os eventos dos botoes'''
         self.tela_cliente.iniciar()
 
+class Controle_tela_administrador_login():
+    '''Classe para controlar a tela de login do administrador'''
+    def __init__(self):
+        self.tela_administrador_login = Tela_administrador_login()
+    def voltar_tela(self):
+        self.tela_administrador_login.janela_administrador_login.destroy()
+    def fazer_login(self):
+        self.tela_administrador_login.janela_administrador_login.destroy()
+        Controle_tela_administrador().start()
+    def start(self):
+        '''Inicia a tela de login do administrador e os eventos dos botoes'''
+        self.tela_administrador_login.btn_voltar_tela.configure(command = self.voltar_tela)
+        self.tela_administrador_login.btn_login.configure(command = self.fazer_login)
+        self.tela_administrador_login.iniciar()
 
+class Controle_tela_bibliotecario_login():
+    '''Classe para controlar a tela de login do bibliotecario'''
+    def __init__(self):
+        self.tela_bibliotecario_login = Tela_bibliotecario_login()
+    def voltar_tela(self):
+        self.tela_bibliotecario_login.janela_bibliotecario_login.destroy()
+    def fazer_login(self):
+        self.tela_bibliotecario_login.janela_bibliotecario_login.destroy()
+        Controle_tela_bibliotecario().start()
+    def start(self):
+        '''Inicia a tela de login do bibliotecario e os eventos dos botoes'''
+        self.tela_bibliotecario_login.btn_voltar_tela.configure(command = self.voltar_tela)
+        self.tela_bibliotecario_login.btn_login.configure(command = self.fazer_login)
+        self.tela_bibliotecario_login.iniciar()
 
+class Controle_tela_cliente_login():
+    '''Classe para controlar a tela de login do cliente'''
+    def __init__(self):
+        self.tela_cliente_login = Tela_cliente_login()
+    def voltar_tela(self):
+        self.tela_cliente_login.janela_cliente_login.destroy()
+    def fazer_login(self):
+        self.tela_cliente_login.janela_cliente_login.destroy()
+        Controle_tela_cliente().start()
+    def start(self):
+        '''Inicia a tela de login do cliente e os eventos dos botoes'''
+        self.tela_cliente_login.btn_voltar_tela.configure(command = self.voltar_tela)
+        self.tela_cliente_login.btn_login.configure(command = self.fazer_login)
+        self.tela_cliente_login.iniciar()
 
 class Controle_tela_inicial():
+    '''Classe para controlar a tela inicial do aplicativo'''
     def __init__(self):
         self.tela = Tela_inicial()
-
     def finalizar_programa(self):
         self.tela.janela_inicial.destroy()
-
-
     def mudar_tela_administrador(self):
-        Controle_tela_administrador().start()
-
+        Controle_tela_administrador_login().start()
     def mudar_tela_cliente(self):
-        Controle_tela_cliente().start()
-
+        Controle_tela_cliente_login().start()
     def mudar_tela_bibliotecario(self):
-        Controle_tela_bibliotecario().start()
-
+        Controle_tela_bibliotecario_login().start()
     def start(self):
-        '''Acoes dos botoes da tela'''
+        '''Inicia a tela inicial e os eventos dos botoes'''
         self.tela.btn_administrador.configure(command = self.mudar_tela_administrador)
         self.tela.btn_bibliotecario.configure(command = self.mudar_tela_bibliotecario)
         self.tela.btn_cliente.configure(command = self.mudar_tela_cliente)
--- tela.py
+++ tela.py
@@ -1,68 +1,82 @@
 from tkinter import Tk, StringVar, Label, Entry, Listbox, Scrollbar, Button, Place
 
 class Tela_inicial():
+    '''Classe que modela a interface grafica da tela inicial'''
     def __init__(self):
         self.janela_inicial = Tk()
         self.janela_inicial.wm_title("DELIBRARY")
         self.janela_inicial.geometry("600x400+100+100")
+        
         self.lb_janela_inicial = Label(self.janela_inicial, text = "ENTRAR NO SISTEMA COMO: ")
+        
         self.btn_administrador = Button(self.janela_inicial, width = 20, text="Administrador")
         self.btn_bibliotecario = Button(self.janela_inicial, width = 20, text="Bibliotecario")
+        
         self.btn_cliente = Button(self.janela_inicial, width = 20, text="Cliente")        
         self.btn_fechar = Button(self.janela_inicial, width = 20, text="Sair")
     def config_layout(self):
+        '''Metodo para configurar os widgets da janela'''
         self.lb_janela_inicial.place(x=0,y=0)
         self.btn_administrador.place(x=0,y=30)
         self.btn_bibliotecario.place(x=0,y=60)
         self.btn_cliente.place(x=0,y=90)
         self.btn_fechar.place(x=0,y=120)
-
     def iniciar(self):
+        '''Metodo para desenhar a janela e processar eventos'''
         self.config_layout()
         self.janela_inicial.mainloop()
         
-class Tela_administrador():
+class Tela_administrador_login():
+    '''Classe que modela a interface grafica da tela de login do administrador'''
     def __init__(self):
-        self.janela_administrador = Tk()
-        self.janela_administrador.geometry("600x400+100+100")
-        self.janela_administrador.wm_title("administrador - DELIBRARY")
+        self.janela_administrador_login = Tk()
+        self.janela_administrador_login.geometry("600x400+100+100")
+        self.janela_administrador_login.wm_title("administrador - DELIBRARY")
+
         self.txt_login = StringVar()
         self.txt_senha = StringVar()
-        self.ent_login = Entry(self.janela_administrador, textvariable=self.txt_login)
-        self.ent_senha = Entry(self.janela_administrador, textvariable=self.txt_senha)       
-        self.btn_voltar_tela = Button(self.janela_administrador, width = 20, text = "Voltar")
-        self.btn_login = Button(self.janela_administrador, width = 20, text = "Entrar")
-        self.lb_janela_administrador_login = Label(self.janela_administrador, text = "LOGIN: ")
-        self.lb_janela_administrador_senha = Label(self.janela_administrador, text = "SENHA: ")
-
+        
+        self.ent_login = Entry(self.janela_administrador_login, textvariable=self.txt_login)
+        self.ent_senha = Entry(self.janela_administrador_login, textvariable=self.txt_senha)       
+        
+        self.btn_voltar_tela = Button(self.janela_administrador_login, width = 20, text = "Voltar")
+        self.btn_login = Button(self.janela_administrador_login, width = 20, text = "Entrar")
+        
+        self.lb_janela_administrador_login = Label(self.janela_administrador_login, text = "LOGIN: ")
+        self.lb_janela_administrador_senha = Label(self.janela_administrador_login, text = "SENHA: ")
     def config_layout(self):
+        '''Metodo para configurar os widgets da janela'''
         self.ent_login.place(x=50,y=1)
         self.ent_senha.place(x=50,y=30)
         self.lb_janela_administrador_login.place(x=0,y=0)
         self.lb_janela_administrador_senha.place(x=0,y=30)
         self.btn_login.place(x=0,y=60)
         self.btn_voltar_tela.place(x=0,y=90)
-
     def iniciar(self):
+        '''Metodo para desenhar a janela e processar eventos'''
         self.config_layout()
-        self.janela_administrador.mainloop() 
+        self.janela_administrador_login.mainloop() 
 
-class Tela_bibliotecario():
+class Tela_bibliotecario_login():
+    '''Classe que modela a interface grafica da tela de login do bibliotecario'''
     def __init__(self):
-        self.janela_bibliotecario = Tk()
-        self.janela_bibliotecario.geometry("600x400+100+100")
-        self.janela_bibliotecario.wm_title("bibliotecario - DELIBRARY")
+        self.janela_bibliotecario_login = Tk()
+        self.janela_bibliotecario_login.geometry("600x400+100+100")
+        self.janela_bibliotecario_login.wm_title("bibliotecario - DELIBRARY")
+
         self.txt_login = StringVar()
         self.txt_senha = StringVar()
-        self.ent_login = Entry(self.janela_bibliotecario, textvariable=self.txt_login)
-        self.ent_senha = Entry(self.janela_bibliotecario, textvariable=self.txt_senha)  
-        self.btn_voltar_tela = Button(self.janela_bibliotecario, width = 20, text = "Voltar")
-        self.btn_login = Button(self.janela_bibliotecario, width = 20, text = "Entrar")
-        self.lb_janela_bibliotecario_login = Label(self.janela_bibliotecario, text = "LOGIN: ")
-        self.lb_janela_bibliotecario_senha = Label(self.janela_bibliotecario, text = "SENHA: ")
 
+        self.ent_login = Entry(self.janela_bibliotecario_login, textvariable=self.txt_login)
+        self.ent_senha = Entry(self.janela_bibliotecario_login, textvariable=self.txt_senha)  
 
+        self.btn_voltar_tela = Button(self.janela_bibliotecario_login, width = 20, text = "Voltar")
+        self.btn_login = Button(self.janela_bibliotecario_login, width = 20, text = "Entrar")
+
+        self.lb_janela_bibliotecario_login = Label(self.janela_bibliotecario_login, text = "LOGIN: ")
+        self.lb_janela_bibliotecario_senha = Label(self.janela_bibliotecario_login, text = "SENHA: ")
     def config_layout(self):
+        '''Metodo para configurar os widgets da janela'''
         self.ent_login.place(x=50,y=1)
         self.ent_senha.place(x=50,y=30)
         self.lb_janela_bibliotecario_login.place(x=0,y=0)
@@ -70,25 +84,30 @@ class Tela_bibliotecario():
         self.btn_voltar_tela.place(x=0,y=90)
         self.btn_login.place(x=0,y=60)
     def iniciar(self):
+        '''Metodo para desenhar a janela e processar eventos'''
         self.config_layout()
-        self.janela_bibliotecario.mainloop()
+        self.janela_bibliotecario_login.mainloop()
          
-class Tela_cliente():
+class Tela_cliente_login():
+    '''Classe que modela a interface grafica da tela de login do cliente'''
     def __init__(self):
-        self.janela_cliente = Tk()
-        self.janela_cliente.geometry("600x400+100+100")
-        self.janela_cliente.wm_title("cliente - DELIBRARY")
+        self.janela_cliente_login = Tk()
+        self.janela_cliente_login.geometry("600x400+100+100")
+        self.janela_cliente_login.wm_title("cliente - DELIBRARY")
+
         self.txt_login = StringVar()
         self.txt_senha = StringVar()
-        self.ent_login = Entry(self.janela_cliente, textvariable=self.txt_login)
-        self.ent_senha = Entry(self.janela_cliente, textvariable=self.txt_senha)  
-        self.btn_voltar_tela = Button(self.janela_cliente, width = 20, text = "Voltar")
-        self.btn_login = Button(self.janela_cliente, width = 20, text = "Entrar")
-        self.lb_janela_cliente_login = Label(self.janela_cliente, text = "LOGIN: ")
-        self.lb_janela_cliente_senha = Label(self.janela_cliente, text = "SENHA: ")
 
+        self.ent_login = Entry(self.janela_cliente_login, textvariable=self.txt_login)
+        self.ent_senha = Entry(self.janela_cliente_login, textvariable=self.txt_senha)  
 
+        self.btn_voltar_tela = Button(self.janela_cliente_login, width = 20, text = "Voltar")
+        self.btn_login = Button(self.janela_cliente_login, width = 20, text = "Entrar")
+
+        self.lb_janela_cliente_login = Label(self.janela_cliente_login, text = "LOGIN: ")
+        self.lb_janela_cliente_senha = Label(self.janela_cliente_login, text = "SENHA: ")
     def config_layout(self):
+        '''Metodo para configurar os widgets da janela'''
         self.ent_login.place(x=50,y=1)
         self.ent_senha.place(x=50,y=30)
         self.lb_janela_cliente_login.place(x=0,y=0)
@@ -96,8 +115,39 @@ class Tela_cliente():
         self.btn_login.place(x=0,y=60)
         self.btn_voltar_tela.place(x=0,y=90)
     def iniciar(self):
+        '''Metodo para desenhar a janela e processar eventos'''
         self.config_layout()
+        self.janela_cliente_login.mainloop()
+
+class Tela_cliente():
+    '''Classe que modela a interface grafica da tela do cliente depois de feito o login'''
+    def __init__(self):
+        self.janela_cliente = Tk()
+        self.janela_cliente.geometry("600x400+100+100")
+        self.janela_cliente.wm_title("cliente - DELIBRARY")
+    def iniciar(self):
+        '''Metodo para desenhar a janela e processar eventos'''
         self.janela_cliente.mainloop()
+
+class Tela_bibliotecario():
+    '''Classe que modela a interface grafica da tela do bibliotecario depois de feito o login'''
+    def __init__(self):
+        self.janela_bibliotecario = Tk()
+        self.janela_bibliotecario.geometry("600x400+100+100")
+        self.janela_bibliotecario.wm_title("bibliotecario - DELIBRARY")
+    def iniciar(self):
+        '''Metodo para desenhar a janela e processar eventos'''
+        self.janela_bibliotecario.mainloop()
+
+class Tela_administrador():
+    '''Classe que modela a interface grafica da tela do administrador depois de feito o login'''
+    def __init__(self):
+        self.janela_administrador = Tk()
+        self.janela_administrador.geometry("600x400+100+100")
+        self.janela_administrador.wm_title("administrador - DELIBRARY")
+    def iniciar(self):
+        '''Metodo para desenhar a janela e processar eventos'''
+        self.janela_administrador.mainloop()
 
 
 
