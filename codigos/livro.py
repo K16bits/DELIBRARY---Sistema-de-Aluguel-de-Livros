ed380eae6d7536027a121b3cd449df2c384a1fe5
fabio vinicius <fabiovinicius1@outlook.com>
2019-11-08 09:26:25 -0400

    Criaçao da classe livro
    Signed-off-by: fabio vinicius <fabiovinicius1@outlook.com>

---
+++ codigos/livro.py
@@ -0,0 +1,9 @@
+class Livro():
+    def __init__(self, id_livro = '', nome = '', genero = '', autor = '',area = '', editora = '', edicao = ''):
+        self.id_livro = id_livro 
+        self.nome = nome
+        self.genero = genero
+        self.autor = autor
+        self.area = area
+        self.editora = editora
+        self.edicao = edicao
